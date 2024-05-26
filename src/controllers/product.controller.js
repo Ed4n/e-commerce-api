493077db100908b5ef4589ec5e9c5eead0028eff
Edgardo Valencia <edgardo.avalencia@gmail.com>
2024-05-26 12:12:28 -0600

    - Find by category - Find by Area - New Products - Product by ID - ALl products names.

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -29,18 +29,121 @@ export const findAll = async (req, res, next) => {
     }
 };
 
-export const findByCategory = async (req, res, next) => {
+export const findOne = async (req, res) => {
+    const { id } = req.params
+
     try {
-        const { categories } = req.query;
-        const selectedCategories = categories.split(',').map(category => category.trim());
-        console.log(selectedCategories)
-        const productsByCategory = await Product.find({ category: { $in: selectedCategories } })
+        const productFound = await Product.findOne({ _id: id })
+
+        if (!productFound) {
+            return res.status(200).json({
+                success: false,
+                message: "Product not found"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Single Product retrieved succesfully",
+            data: productFound
+        })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({
+            Error: "Internal server error",
+            details: err
+        })
+    }
+}
+
+export const findNew = async (req, res) => {
+    const limit = req.query.limit
+    const limitValue = limit ? parseInt(limit, 10) : null
+
+    try {
+        const query = Product.find({ new: true })
+
+        if (limitValue) {
+            query.limit(limitValue)
+        }
+
+        const newProducts = await query.exec()
+        const newProductsLenght = newProducts.length.toString()
+
+        return res.status(200).json({
+            success: true,
+            message: "New Products retrieve succesfully",
+            data: newProducts,
+            count: newProductsLenght
+        })
+
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            details: err
+        })
+    }
+}
+
+export const findAllProductNames = async (req, res) => {
+    try {
+        // Find all products and project only the 'name' field
+        const products = await Product.find({}, 'name');
+
+        // Extract just the names from the products
+        const productNames = products.map(product => product.name);
+        const productsNmaesLength = productNames.length.toString()
+
+        // Send the response with the list of product names
+        return res.status(200).json({
+            success: true,
+            message: "Product names retrieved successfully",
+            data: productNames,
+            count: productsNmaesLength
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            error: "Internal server error",
+            details: err.message,
+        });
+    }
+}
+
+export const findByCategory = async (req, res, next) => {
+    const limit = req.query.limit
+    const limitValue = limit ? parseInt(limit, 10) : null
+
+    const { categories } = req.query;
+
+    if (!categories) {
+        return res.status(400).json({
+            success: false,
+            message: "Categories parameter is required."
+        })
+    }
+
+    const selectedCategories = categories.split(',').map(category => category.trim());
+
+    try {
+        const query = Product.find({ category: { $in: selectedCategories } })
+
+        if (limitValue) {
+            query.limit(limitValue)
+        }
+
+        const productsByCategory = await query.exec()
+        const productsByCategoryLength = productsByCategory.length.toString()
 
         if (productsByCategory.length > 0) {
             return res.status(200).json({
                 success: true,
                 message: "Products by Cateogries retrieve succesfully!",
-                data: productsByCategory
+                data: productsByCategory,
+                count: productsByCategoryLength
             })
         } else {
             return res.status(404).json({
@@ -52,6 +155,51 @@ export const findByCategory = async (req, res, next) => {
 
     } catch (err) {
         next(err)
+    }
+}
+
+export const findByArea = async (req, res, next) => {
+
+    const limit = req.query.limit
+    const limitValue = limit ? parseInt(limit, 10) : null
+    const { areas } = req.query;
+
+    if (!areas) {
+        return res.status(400).json({
+            success: false,
+            message: "Areas parameter is required"
+        })
+    }
+
+    const selectedAreas = areas.split(',').map(area => area.trim());
+
+    try {
+
+        const query = Product.find({ area: { $in: selectedAreas } })
+
+        if (limitValue) {
+            query.limit(limitValue)
+        }
+
+        const productsByArea = await query.exec()
+        const productsByAreaLength = parseInt(productsByArea.length, 10)
+
+        if (productsByArea.length > 0) {
+            return res.status(200).json({
+                success: true,
+                message: "Products by Cateogries retrieve succesfully!",
+                data: productsByArea,
+                count: productsByAreaLength
+            })
+        } else {
+            return res.status(404).json({
+                success: false,
+                message: 'No products found for the specified categories.'
+            });
+        }
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ error: "Internal server error", details: err });
     }
 }
 
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express'
-import { findAll, findByCategory } from '../controllers/product.controller.js'
+import { findAll, findAllProductNames, findByArea, findByCategory, findNew, findOne } from '../controllers/product.controller.js'
 
 const router = Router()
 
-router.get("/", findAll)
 router.get("/categories", findByCategory)
+router.get("/areas", findByArea)
+router.get("/new", findNew)
+
+router.get("/", findAll)
+router.get("/products-names", findAllProductNames)
+router.get("/:id", findOne)
+
 
 export default router;
